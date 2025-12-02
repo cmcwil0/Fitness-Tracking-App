74020cf1e7443089dcee190e29764d16821b1093
cmcwil0 <cmcwil0@gmail.com>
2025-12-01 18:47:33 -0600

    added styles

--- src/components/TotalTimeCard.jsx
+++ src/components/TotalTimeCard.jsx
@@ -16,7 +16,7 @@ const TotalTimeCard = () => {
   return (
     <div className={classes.totalTimeContainer}>
       <div className={classes.header}>
-        <div className={classes.icon}>⏱️</div>
+        <div className={classes.icon}><svg xmlns="http://www.w3.org/2000/svg" width='20px' height='20px' fill='var(--accent-color)' viewBox="0 0 512 512"><path d="M256 0a256 256 0 1 1 0 512 256 256 0 1 1 0-512zM232 120l0 136c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2 280 120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg></div>
         <div className={classes.title}>Total Time</div>
       </div>
 
