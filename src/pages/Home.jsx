08f03da9bab4754e7a2c59fad0117cc69f9265b4
cmcwil0 <111789393+cmcwil0@users.noreply.github.com>
2025-12-03 20:21:48 -0600

    Altered home.jsx and home.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
-import '../css/Home.css'
-import { Link } from 'react-router-dom'
-import { isLoggedIn } from '../utils/auth'
+import '../css/Home.css';
+import { Link } from 'react-router-dom';
+import { isLoggedIn } from '../utils/auth';
 
 const Home = () => {
   const loggedIn = isLoggedIn();
 
   return (
     <div className='home-page'>
+      <div className='background-image'></div>
       <h1 className='main-heading'>All in One Fitness and Nutrition Tracker</h1>
+      <p className='about-info'>Track your workouts, monitor your nutrition, and achieve your fitness goals all in one place. Join our community of fitness enthusiasts today!</p>
+      <br></br>
       <button className='start-button'>
         {loggedIn ? (
           <Link to="/dashboard">View Dashboard</Link>
@@ -15,8 +18,9 @@ const Home = () => {
           <Link to="/login">Get Started Today</Link>
         )}
       </button>
-    </div>
-  )
-}
 
-export default Home
+    </div>
+  );
+};
+
+export default Home;
